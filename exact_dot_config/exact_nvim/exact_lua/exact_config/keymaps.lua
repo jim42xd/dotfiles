9d09e6a729b5fe2cb56f962109e2c3092623b2c9
Anuar Lomas <anuarl@rawpowergames.com>
2025-06-02 18:22:19 +0200

    Added aerospace config

--- exact_dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
+++ exact_dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
@@ -4,4 +4,9 @@ vim.g.maplocalleader = " "
 
 -- General
 vim.keymap.set({ "n", "v" }, "<leader>p", '"_dP', { desc = "Paste and retain" })
-vim.keymap.set("n", "<esc>", ":nohl<CR>", { desc = "Remove highlight" })
+vim.keymap.set("n", "<esc>", ":nohl<CR>", { desc = "Remove highlight", silent = true })
+vim.keymap.set("n", "U", "<c-r>", { desc = "Nicer re-do" })
+
+-- Improve navigation
+vim.keymap.set("n", "{", "}")
+vim.keymap.set("n", "}", "{")
